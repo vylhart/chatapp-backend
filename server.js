c13e4609ee13eedde4c9970cc6ddf45460351bb6
Shashank <shashankgangwar12@gmail.com>
2023-07-08 19:46:20 +0530

    add follow api

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const dotenv = require('dotenv');
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const {initSocket} = require('./lib/socket');
-const {connectToMongoDB, Tweet, User, Credential} = require('./lib/db');
+const {connectToMongoDB, Tweet, User, Credential, Follow} = require('./lib/db');
 const {configureTweetDB} = require('./lib/tweetDB');
 const {configureUserDB} = require('./lib/UserDB');
+const {configureRelationshipApi} = require("./lib/relationshipApi");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -24,6 +25,7 @@ connectToMongoDB()
 initSocket(server);
 configureTweetDB(app, Tweet);
 configureUserDB(app, Credential, User);
+configureRelationshipApi(app, Follow);
 
 app.get('/hello', (req, res) => {
     console.log('hello');
